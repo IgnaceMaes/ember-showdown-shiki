69208f9f63b2bd4ad03b747b7b61766bd40c96c9
Ignace Maes <contact@ignacemaes.com>
2024-06-29 16:24:47 +0200

    test: check on dom element

--- test-app/tests/integration/components/markdown-to-html-test.ts
+++ test-app/tests/integration/components/markdown-to-html-test.ts
@@ -1,14 +1,22 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'test-app/tests/helpers';
-import { render } from '@ember/test-helpers';
+import { find, render, type TestContext } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+
+interface Context extends TestContext {
+  markdown: string;
+}
 
 module('Integration | Component | markdown-to-html', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders text', async function (assert) {
-    await render(hbs`<MarkdownToHtml @markdown='Hello world!' />`);
+  test('it renders markdown text', async function (this: Context, assert) {
+    this.set('markdown', '*hello world*');
+    await render(hbs`<MarkdownToHtml @markdown={{this.markdown}} />`);
 
-    assert.dom().hasText('Hello world!');
+    assert.strictEqual(
+      find('div')!.innerHTML.trim(),
+      '<p><em>hello world</em></p>',
+    );
   });
 });

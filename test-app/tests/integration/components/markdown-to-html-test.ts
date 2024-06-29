fc138010f9bed5973328348e5675915f5176c8fe
Ignace Maes <contact@ignacemaes.com>
2024-06-29 16:26:50 +0200

    test: code test

--- test-app/tests/integration/components/markdown-to-html-test.ts
+++ test-app/tests/integration/components/markdown-to-html-test.ts
@@ -19,4 +19,14 @@ module('Integration | Component | markdown-to-html', function (hooks) {
       '<p><em>hello world</em></p>',
     );
   });
+
+  test('it renders markdown code', async function (this: Context, assert) {
+    this.set('markdown', '```js\nconsole.log("hello world");\n```');
+    await render(hbs`<MarkdownToHtml @markdown={{this.markdown}} />`);
+
+    assert.strictEqual(
+      find('div')!.innerHTML.trim(),
+      '<pre class=\"shiki github-dark\" style=\"background-color:#1c1e24;color:#e1e4e8\" tabindex=\"0\"><code class=\"language-js line-numbers\"><span class=\"line\"><span style=\"color:#E1E4E8\">console.</span><span style=\"color:#B392F0\">log</span><span style=\"color:#E1E4E8\">(</span><span style=\"color:#9ECBFF\">\"hello world\"</span><span style=\"color:#E1E4E8\">);</span></span></code></pre>',
+    );
+  });
 });
